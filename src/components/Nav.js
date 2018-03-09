df0e9359aee54b0f70661c349eacd2342143789f
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-09 21:07:56 +1100

    Add Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => (
+  <nav class="navbar navbar-dark bg-pink justify-content-start">
+    <a class="nav-link active text-white border-bottom" href="#">Active</a>
+    <a class="nav-link text-white" href="#">Link</a>
+    <a class="nav-link text-white" href="#">Link</a>
+    <a class="nav-link disabled text-dark" href="#">Disabled</a>
+  </nav>
+)
+
+export default Nav
