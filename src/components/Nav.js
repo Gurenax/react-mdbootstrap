1c06b9340a89f7aeb61d260e3d41d3fe5935640a
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-09 21:16:30 +1100

    Update class to className

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Nav = () => (
-  <nav class="navbar navbar-dark bg-pink justify-content-start">
-    <a class="nav-link active text-white border-bottom" href="/">Active</a>
-    <a class="nav-link text-white" href="/">Link</a>
-    <a class="nav-link text-white" href="/">Link</a>
-    <a class="nav-link disabled text-dark" href="/">Disabled</a>
+  <nav className="navbar navbar-dark bg-pink justify-content-start">
+    <a className="nav-link active text-white border-bottom" href="/">Active</a>
+    <a className="nav-link text-white" href="/">Link</a>
+    <a className="nav-link text-white" href="/">Link</a>
+    <a className="nav-link disabled text-dark" href="/">Disabled</a>
   </nav>
 )
 
