4ff0c22f2d0ed5b17de554f15b4f97fccf97b9a1
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-09 21:23:19 +1100

    Update organisation of bootstrap pre-requisites

---
+++ src/include/bootstrap.js
@@ -0,0 +1,7 @@
+import './jquery'
+import './tether'
+import './popper'
+
+import 'bootstrap'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/mdbootstrap/css/mdb.min.css'
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/mdbootstrap/css/mdb.min.css'
+import './include/bootstrap'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
