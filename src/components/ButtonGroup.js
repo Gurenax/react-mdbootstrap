fddeb9933e44e4499f73da6cf30da6699ff94708
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-09 22:14:14 +1100

    Fit button margins

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -4,7 +4,7 @@ const ButtonGroup = () => (
   <div className="btn-group" role="group" aria-label="Basic example">
     <button type="button" className="btn mr-1 btn-pink">Left</button>
     <button type="button" className="btn mr-1 btn-pink">Middle</button>
-    <button type="button" className="btn btn-pink">Right</button>
+    <button type="button" className="btn mr-1 btn-pink">Right</button>
   </div>
 )
 
--- src/components/ButtonToolbar.js
+++ src/components/ButtonToolbar.js
@@ -3,19 +3,20 @@ import React from 'react'
 const ButtonToolbar = () => (
   <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
     <div className="btn-group mr-2" role="group" aria-label="First group">
-      <button type="button" className="btn mr-1 btn-pink">1</button>
-      <button type="button" className="btn mr-1 btn-pink">2</button>
-      <button type="button" className="btn mr-1 btn-pink">3</button>
-      <button type="button" className="btn mr-1 btn-pink">4</button>
+      <button type="button" className="btn mr-1 mb-1 btn-pink">1</button>
+      <button type="button" className="btn mr-1 mb-1 btn-pink">2</button>
+      <button type="button" className="btn mr-1 mb-1 btn-pink">3</button>
+      <button type="button" className="btn mr-1 mb-1 btn-pink">4</button>
     </div>
     <div className="btn-group mr-2" role="group" aria-label="Second group">
-      <button type="button" className="btn mr-1 btn-pink">5</button>
-      <button type="button" className="btn mr-1 btn-pink">6</button>
-      <button type="button" className="btn btn-pink">7</button>
+      <button type="button" className="btn mr-1 mb-1 btn-pink">5</button>
+      <button type="button" className="btn mr-1 mb-1 btn-pink">6</button>
+      <button type="button" className="btn mr-1 mb-1 btn-pink">7</button>
+      <button type="button" className="btn mr-1 mb-1 btn-pink">8</button>
     </div>
-    <div className="btn-group" role="group" aria-label="Third group">
-      <button type="button" className="btn btn-pink">8</button>
-    </div>
+    {/* <div className="btn-group" role="group" aria-label="Third group">
+      <button type="button" className="btn mb-1 btn-pink">8</button>
+    </div> */}
   </div>
 )
 
