56a5d1e4a7cfa735d964e6474242146dfb1cae00
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-09 21:14:12 +1100

    Update link href

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Nav = () => (
   <nav class="navbar navbar-dark bg-pink justify-content-start">
-    <a class="nav-link active text-white border-bottom" href="#">Active</a>
-    <a class="nav-link text-white" href="#">Link</a>
-    <a class="nav-link text-white" href="#">Link</a>
-    <a class="nav-link disabled text-dark" href="#">Disabled</a>
+    <a class="nav-link active text-white border-bottom" href="/">Active</a>
+    <a class="nav-link text-white" href="/">Link</a>
+    <a class="nav-link text-white" href="/">Link</a>
+    <a class="nav-link disabled text-dark" href="/">Disabled</a>
   </nav>
 )
 
