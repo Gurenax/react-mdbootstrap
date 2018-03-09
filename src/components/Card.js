fc227cfc5ed64af373f06514f30353fa2cbf130e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-09 21:14:06 +1100

    Update image location and link

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Card = () => (
   <div className="card" style={{'width': 18+'rem'}}>
-    <img className="card-img-top" src="https://img.heypik.com/58pic/27/91/98/32358PICHdF.jpg?x-oss-process=image/resize,w_1024" alt="Card image cap"/>
+    <img className="card-img-top" src="assets/images/sample.jpg" alt="Card caption"/>
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="btn btn-block btn-pink">Go somewhere</a>
+      <a href="/" className="btn btn-block btn-pink">Go somewhere</a>
     </div>
   </div>
 )
