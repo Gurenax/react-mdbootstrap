f234488014fae07540fa0601c2aad1b675bf0a94
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-14 12:34:56 +1100

    Fix missing bootstrap import

--- src/include/bootstrap.js
+++ src/include/bootstrap.js
@@ -2,5 +2,6 @@ import './jquery'
 import './tether'
 import './popper'
 
+import 'bootstrap'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/mdbootstrap/css/mdb.min.css'
