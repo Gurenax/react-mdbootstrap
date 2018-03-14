684599868672e61aa26eb6abe69ab13362c6348d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-14 12:44:27 +1100

    Add headings, paragraph, card deck

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Nav from './components/Nav'
 import Form from './components/Form'
 import Card from './components/Card'
+import CardDeck from './components/CardDeck'
 import DropdownButton from './components/DropdownButton'
 import ToggleSwitch from './components/ToggleSwitch'
 import ButtonGroup from './components/ButtonGroup'
@@ -17,36 +18,55 @@ class App extends Component {
     return (
       <div className="App">
         <Nav />
-        <div className="container mt-3">
+        <div className="container mt-5">
           <h1>Material Bootstrap 4</h1>
           <Jumbotron/>
 
-          <h3 className="mt-3">Dropdown Button</h3>
+          <h3 className="mt-5">Roboto Headings</h3>
+          <h1>Heading 1</h1>
+          <h2>Heading 2</h2>
+          <h3>Heading 3</h3>
+          <h4>Heading 4</h4>
+          <h5>Heading 5</h5>
+
+          <h3 className="mt-5">Roboto Paragraph</h3>
+          <p>Food truck helvetica YOLO pabst man braid sriracha. Meditation skateboard brooklyn edison bulb street art paleo hell of try-hard. Ugh chillwave ennui gochujang cred slow-carb 3 wolf moon thundercats taxidermy vexillologist. Post-ironic polaroid meh bitters offal enamel pin, hoodie banh mi. Waistcoat man bun woke chartreuse you probably haven't heard of them, listicle chicharrones etsy whatever trust fund dreamcatcher yuccie. Microdosing narwhal affogato, tbh taxidermy ugh twee. Palo santo craft beer butcher, single-origin coffee subway tile salvia cray distillery bicycle rights scenester sustainable cornhole polaroid.</p>
+          
+
+          <h3 className="mt-5">Dropdown Button</h3>
           <DropdownButton />
 
-          <h3 className="mt-3">Button Group</h3>
+          <h3 className="mt-5">Button Group</h3>
           <ButtonGroup />
 
-          <h3 className="mt-3">Button Toolbar</h3>
+          <h3 className="mt-5">Button Toolbar</h3>
           <ButtonToolbar />
 
-          <h3 className="mt-3">Toggle Switch</h3>
+          <h3 className="mt-5">Toggle Switch</h3>
           <ToggleSwitch />
           
-          <h3 className="mt-3">Form</h3>
+          <h3 className="mt-5">Form</h3>
           <Form />
 
-          <h3 className="mt-3">Custom Checkbox</h3>
+          <h3 className="mt-5">Custom Checkbox</h3>
           <CustomCheckbox />
 
-          <h3 className="mt-3">Card</h3>
+          <h3 className="mt-5">Card</h3>
           <Card />
+          
+          <h3 className="mt-5">Card Deck</h3>          
+          <CardDeck>
+            <Card />
+            <Card />
+            <Card />
+          </CardDeck>
 
-          <h3 className="mt-3">Collapse</h3>
+          <h3 className="mt-5">Collapse</h3>
           <Collapse />
 
-          <h3 className="mt-3">Pagination</h3>
+          <h3 className="mt-5">Pagination</h3>
           <Pagination />
+          
         </div>
       </div>
     )
